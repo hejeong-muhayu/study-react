9621c9128ae7a62996979af02db0325715d2ec6b
h1un <h1unha@naver.com>
2024-05-15 18:11:47 +0900

    섹션 13 : Composition vs Inheritance 실습
    Card 컴포넌트 만들기

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card"
+
+function ProfileCard(props){
+
+    return(
+    <Card title={"inje lee"}
+          backgroundColor={"green"}>
+        <p>안녕하세요 소플입니다.</p>
+        <p>저는 리액트를 개발하고 있습니다.</p>
+    </Card>
+    )
+}
+
+export default ProfileCard;
