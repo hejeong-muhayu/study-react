d5b520d728b1a5944d2ee635f4995e7751a64c12
h1un <h1unha@naver.com>
2024-05-15 16:37:21 +0900

    섹션 9 : Conditional Rendering (조건부 렌더링) 실습
    inline Conditions 과
    inline if-else(3항 연산)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ConfirmButton from "./chapter_08/ConfirmButton";
+import LendingPage from "./chapter_09/LendingPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LendingPage />
   </React.StrictMode>
 );
 
