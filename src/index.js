f7660b69154689a9e16db8a544f760752848e810
h1un <h1unha@naver.com>
2024-05-16 22:27:18 +0900

    섹션 16 : 미니 프로젝트 - 블로그 만들기

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Blocks from "./chapter_15/Blocks"
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blocks/>
+    <App/>
   </React.StrictMode>
 );
 
