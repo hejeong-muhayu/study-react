1b63f856055d9d97dd113401023de19fab080d6d
h1un <h1unha@naver.com>
2024-05-17 16:23:56 +0900

    1-9 다중 input 상태 관리

--- react-vlpt/src/inputSample.jsx
+++ react-vlpt/src/inputSample.jsx
@@ -2,22 +2,37 @@ import React, {useState} from 'react';
 
 function InputSample(props) {
 
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name:'',
+        nickname:''
+    });
 
-    const onChange = (e) =>{
-        setText(e.target.value);
-    }
+    const {name, nickname} = inputs;
+
+    const onChange = (e) => {
+        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+        setInputs({
+            ...inputs, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
+        });
+    };
+
 
     const onReset = (e) =>{
-        setText('');
+        setInputs({
+            name:'',
+            nickname: ''
+        })
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input placeholder={"이름"} />
+            <input name="name" placeholder={"이름"} onChange={onChange} value={name}/>
+            <input name="nickname" placeholder={"닉네임"} onChange={onChange} value={nickname}/>
             <button onClick={onReset}> 초기화 </button>
             <div>
-                <b> 값 : {text}</b>
+                <b> 값 : {name} ({nickname})</b>
             </div>
         </div>
     );
