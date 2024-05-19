7e052a73e62b1d51bb4fa14a1d511d75de79819d
h1un <h1unha@naver.com>
2024-05-20 00:35:59 +0900

    4-3. useAsync 커스텀 hook 만들어 사용

--- react-vlpt/api-integrate/src/Users..jsx
+++ react-vlpt/api-integrate/src/Users..jsx
@@ -1,57 +1,21 @@
-import React, {useEffect, useReducer} from 'react';
+import React from 'react';
 import axios from "axios";
+import useAsync from "./useAsync";
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null
-            };
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null
-            };
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error
-            };
-        default :
-            throw new Error(`Unhandled action type : ${action.type}`);
-    }
+// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
+// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
+async function getUsers() {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    return response.data;
 }
+
 
 function Users(props) {
 
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null
-    });
-
-    const fetchUsers = async () => {
-        dispatch({type: 'LOADING'});
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users'
-            );
-            dispatch({type: 'SUCCESS', data: response.data});
-
-        } catch (e) {
-            dispatch({type: 'ERROR', error: e})
-        }
-    }
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
-    const {loading, data: users, error} = state;
+    const [state, refetch] = useAsync(getUsers, [])
+    const {loading, data: users, error} = state; // state.data 를 users 키워드로 조회
 
     if (loading)
         return <div>로딩중..</div>;
@@ -61,13 +25,16 @@ function Users(props) {
         return null;
 
     return (
-        <ul>
-            {users.map(user => (
-                <li key={user.id}>
-                    {user.username} ({user.name})
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={refetch}>다시 불러오기</button>
+        </>
     );
 }
 
