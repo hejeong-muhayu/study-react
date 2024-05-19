2afdbc55cf6494dcdc4d0ac068032bdbdf32f859
h1un <h1unha@naver.com>
2024-05-20 00:31:16 +0900

    4-2. useState -> useReducer 로 변경하여 상태 관리

--- react-vlpt/api-integrate/src/Users..jsx
+++ react-vlpt/api-integrate/src/Users..jsx
@@ -1,37 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import axios from "axios";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            };
+        default :
+            throw new Error(`Unhandled action type : ${action.type}`);
+    }
+}
 
 function Users(props) {
 
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null
+    });
+
+    const fetchUsers = async () => {
+        dispatch({type: 'LOADING'});
+        try {
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users'
+            );
+            dispatch({type: 'SUCCESS', data: response.data});
+
+        } catch (e) {
+            dispatch({type: 'ERROR', error: e})
+        }
+    }
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setError(null);
-                setUsers(null);
-
-                setLoading(true);
-
-                const response = await axios.get(
-                    'https://jsonplaceholder.typicode.com/users'
-                )
-
-                setUsers(response.data);
-
-            } catch (e) {
-                setError(e);
-            }
-
-            setLoading(false);
-        };
-
         fetchUsers();
-
     }, []);
 
+    const {loading, data: users, error} = state;
 
     if (loading)
         return <div>로딩중..</div>;
