84a878891db7cbeb490de2d46230fdbfd3d03d32
h1un <h1unha@naver.com>
2024-08-07 20:54:04 +0900

    공식문서 읽기 > ui 표현하기 > JSX로 마크업 작성하기
    TodoList.jsx

--- official-document/src/App.jsx
+++ official-document/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Gallery from "./ui표현하기/Gallery.jsx";
+import TodoList from "./ui표현하기/TodoList.jsx";
 
 function App() {
 
   return (
     <>
         <Gallery/>
+        <TodoList/>
     </>
   )
 }
