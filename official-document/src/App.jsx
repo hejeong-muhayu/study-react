868bfa1e4731a49aeb280d636840b96c6748e5ee
h1un <h1unha@naver.com>
2024-08-08 21:45:54 +0900

    공식문서 읽기 > ui 표현하기 > 조건부 렌더링
    Avatar.jsx Profile.jsx

--- official-document/src/App.jsx
+++ official-document/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Gallery from "./ui표현하기/Gallery.jsx";
 import TodoList from "./ui표현하기/TodoList.jsx";
 import Profile from "./ui표현하기/Profile.jsx";
+import PackingList from "./ui표현하기/PackingList.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Gallery/>
         <TodoList/>
         <Profile/>
+        <PackingList/>
     </>
   )
 }
